e45d3c0163a1fc1b8355c1061d34b8c1bd50ba37
raafa16 <raafa16@hotmail.com>
2016-09-28 13:09:16 +0600

    Validation with Fixtures!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,6 +4,7 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :products
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -49,5 +50,13 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
 
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                        description: "yyy",
+                        price: 1,
+                        image_url: "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join(';')
+  end
 
 end
