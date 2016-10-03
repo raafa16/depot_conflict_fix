489a1799735353bbe0695ef7777e544507a38baa
raafa16 <raafa16@hotmail.com>
2016-10-03 13:13:52 +0600

    Checkout feature added

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -27,7 +27,7 @@ class ProductTest < ActiveSupport::TestCase
         product.errors[:price].join(';')
 
     product.price=1
-    assert product.invalid?
+    assert product.valid?
   end
   def new_product(image_url)
     Product.new(title: "My Book Title",
