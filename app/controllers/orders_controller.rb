5c2be5d695ca770b974f7af990847309028722ed
raafa16 <raafa16@hotmail.com>
2016-10-04 10:00:19 +0600

    Sending confirmation Emails

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,6 +40,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 'Order was successfully created. Thankyou!' }
         format.json { render :show, status: :created, location: @order }
       else
