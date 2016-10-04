0d4772ccd7a5f64dcaa0d21b33662ac818ac83c0
raafa16 <raafa16@hotmail.com>
2016-10-04 18:20:50 +0600

    Checkout Translated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to store_url, notice: 'Order was successfully created. Thankyou!' }
+        format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render :show, status: :created, location: @order }
       else
         @cart = current_cart
