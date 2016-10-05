bf96aa58b092387f153dc91abb4a594c775200a9
raafa16 <raafa16@hotmail.com>
2016-10-05 18:21:17 +0600

    Discount migration Tested

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
-
+   has_many :discounts
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
